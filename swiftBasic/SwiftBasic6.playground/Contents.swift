bd44f71486c9a90bf18fc6c442890418dc44b242
sunghunchoi <csh6222sp@gmail.com>
2019-04-12 20:50:49 +0900

    클로져,프로퍼티

--- swiftBasic/SwiftBasic6.playground/Contents.swift
+++ swiftBasic/SwiftBasic6.playground/Contents.swift
@@ -148,16 +148,162 @@ print(result5)
 result5 = cl5(a: 20, b: 30){$0 * $1}
 print(result5)
 
+/*
+    1.프로퍼티
+      인스턴스 저장 프로퍼티
+      타입 저장 프로퍼티
+      인스턴스 연산 프로퍼티
+      타입 연산 프로퍼티
+      지연 저장 프로퍼티
+ 
+    2.정의와 사용
+      프로퍼티는 구조체,클래스,열거형 내부에 구현할 수 있음.
+      다만 열거형 내부에는 연산 프로퍼티만 구현할 수 있음
+      연산 프로퍼티는 var로만 선언할 수 있음.
+      연산프로퍼티를 읽기전용으로는 구현할 수 있지만, 쓰기 전용으로는 구현할 수 없음.
+      읽기전용으로 구현하라면 get 블럭만 작성해주면 됨. 읽기전용은 get블럭을 생략할 수 있음.
+      읽기,쓰기 모두 가능하게 하려면 get블럭과 set블럭을 모두 구현
+      set 블럭에서 암시적 매개변수 newValue를 사용할 수 있음.
+ */
 
+struct Student{
+    // 인스턴스 저장 프로퍼티
+    var name:String = ""
+    var `class`:String = "Swift"
+    var koreanAge:Int = 0
+    
+    // 인스턴스연산 프로퍼티
+    var westernAge:Int{
+        get{
+            return koreanAge-1
+        }
+        set(InputValue){
+            koreanAge = InputValue+1
+        }
+    }
+    
+    // 타입 저장 프로퍼티
+    static var typeDescription:String="학생"
+    
+    /*
+        인스턴스 메서드
+     func selfIntroduce(){
+        print("저는 \(self.class)반 \(name)입니다.
+     }
+     */
+    // 읽기전용 인스턴스 연산 프로퍼티
+    var selfIntroduction:String{
+        get{
+            return "저는 \(self.class)반 \(name)입니다."
+        }
+    }
+    
+    /*
+        //타입 메서드
+     static func selfIntrocue(){
+        print("학생입니다.")
+     }
+     */
+    // 읽기전용 타입 연산 프로퍼티
+    // 읽기전용에서는 get을 생략할 수 있습니다.
+    static var selfIntroduce:String{
+        return "학생입니다."
+    }
+}
 
+// 타입 연산 프로퍼티 사용
+print(Student.selfIntroduce)
 
+// 인스턴스 생성
+var show:Student = Student()
+show.koreanAge=20
 
+// 인스턴스 저장 프로퍼티 사용
+show.name = "show"
+print(show.name)
 
+// 인스턴스 연산 프로퍼티 사용
+print(show.selfIntroduction)
+print("제 한국나이는 \(show.koreanAge)살이고, 미국나이는 \(show.westernAge)살입니다.")
 
+// 응용
+struct Money{
+    var currentRate:Double = 1100
+    var dollar:Double = 0
+    var won:Double{
+        get{
+            return dollar * currentRate
+        }
+        set (newInputValue){
+            dollar = newInputValue/currentRate
+        }
+    }
+}
 
+var moneyInMoney = Money()
 
+moneyInMoney.won = 11000
 
+print(moneyInMoney.won)
+moneyInMoney.dollar = 10
+print(moneyInMoney.won)
 
+// 지역변수 및 전역변수
+// 저장 프로퍼티 연산 프로퍼티의 기능은 함수, 메서드, 클로저, 타입 등의 외부에 위치한 지역/전역 변수에도 모두 사용 가능하다.
+var a:Int = 100
+var b:Int = 200
+var Sum:Int{
+    return a+b
+}
+print(sum)
+
+/*
+    프로퍼티 감시자
+     -> 프로퍼티 감시자를 사용하면 프로퍼티의 값이 변경될 때 원하는 동작을 수행할 수 있음.
+     -> 값이 변경되기 직전에 willSet블럭이, 값이 변경된 직후에 didSet블럭이 호출됨.
+     -> 둘 중 필요한 하나만 구현해 주어도 무관함.
+     -> 변경되려는 값이 기존의 값과 똑같더라도 프로퍼티 감시자는 항상 동작함.
+     -> willSet 블럭에서는 암시적 매개변수 newValue를, didSet블럭에서는 oldValue를 사용할 수 있음.
+     -> 프로퍼티 감시자는 연산 프로퍼티에는 사용할 수 없음.
+     -> 프로퍼티 감시자는 함수,메서드,클로저,타입 등의 지역/전역 변수에 모두 사용 가능.
+ */
+
+struct Money2{
+    // 프로퍼티 감시자 사용
+    var currentRate:Double = 1100{
+        willSet(newRate){
+            print("환율이 \(currentRate)에서 \(newRate)으로 변경될 예정입니다.")
+        }
+        
+        didSet(oldRate){
+            print("환율이 \(oldRate)에서 \(currentRate)으로 변경되었습니다.")
+        }
+    }
+    
+    var dollar:Double = 0{
+        willSet{
+            print("\(dollar)달러에서 \(newValue)달러로 변경될 예정입니다.")
+        }
+        didSet{
+            print("\(oldValue)달러에서 \(dollar)달러로 변경될 변경되었습니다.")
+        }
+    }
+    
+    var won:Double{
+        get{
+            return dollar * currentRate
+        }
+        set{
+            dollar = newValue / currentRate
+        }
+    }
+}
+
+var moneyInMyPocket2:Money2=Money2()
+
+moneyInMyPocket2.currentRate = 1150
+moneyInMyPocket2.dollar = 10
+print(moneyInMyPocket2.won)
 
 
 
